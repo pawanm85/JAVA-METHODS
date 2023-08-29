4520647a9aa85f7af1a9f08b419dd3819fac0bbe
Pawan maske <112793534+pawanm85@users.noreply.github.com>
2023-08-29 20:49:24 +0530

    Add files via upload

---
+++ Condi1.java
@@ -0,0 +1,15 @@
+class Condi1
+{
+public static void main(String[]args)
+{
+char ch1='a'; char ch2='b';
+// int n1=80;
+// int n2=70;
+// int n3=90;
+//int ans=n1>(n2>n3?n2:n3)?n1:(n2>n3?n2:n3);
+//int ans=(n1>n2)?(n1>n3?n1:n3):(n2>n3?n2:n3);
+char ans=ch1<ch2?ch1:ch2;
+System.out.println(ans);
+}
+
+}
